9aac31176870fa12efa395cf1e55d8607ae656cf
Aditya Pallerlamudi <stp.abhi@gmail.com>
2018-07-05 20:07:31 -0700

    Aditya: VC changes

--- WallChanger/ViewController.swift
+++ WallChanger/ViewController.swift
@@ -12,14 +12,27 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        registerSettingsBundle()
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.defaultsChanged), name: UserDefaults.didChangeNotification, object: nil)
+        defaultsChanged()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func registerSettingsBundle(){
+        let appDefaults = [String:AnyObject]()
+        UserDefaults.standard.register(defaults: appDefaults)
     }
-
+    @objc func defaultsChanged(){
+        if UserDefaults.standard.bool(forKey: "RedThemeKey") {
+            self.view.backgroundColor = UIColor.red
+            
+        }
+        else {
+            self.view.backgroundColor = UIColor.green
+        }
+    }
+    
+    deinit { //Not needed for iOS9 and above. ARC deals with the observer in higher versions.
+        NotificationCenter.default.removeObserver(self)
+    }
 
 }
 
